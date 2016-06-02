0758fad58949f581c11c8f9f7f69d8cbcb0ceb0f
aquairus <18650013469@163.com>
2016-06-02 11:18:59 +0800

    save

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import cd,run,env,hosts,roles,execute,settings,local
+import os
+
+
+
+def save():
+	local("git add -A")
+	with settings(warn_only=True):
+		local("git commit -m 'save' ")
+		local("git push")
+
+
+def dep():
+	with cd("/data/src"):
+		run ("git pull")
